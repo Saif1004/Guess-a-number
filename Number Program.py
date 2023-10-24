1cd7685028bf91812183611d77e94811eecb5220
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2023-10-25 00:56:22 +0100

    Working Program

---
+++ Number Program.py
@@ -0,0 +1,19 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input("Guess the number: "))
+        if guess> random_number:
+            print(f"Sorry {guess} is too high. Try again.")
+        elif guess< random_number:
+            print(f"Sorry {guess} is too low")
+
+    print(f"{guess} is correct")
+
+guess(10)
+
+
+
+
